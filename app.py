b1900e6144a876ec006c5fa2de3373f8a495f350
Ishita chaturvedi <168914437+ishita-20-6@users.noreply.github.com>
2026-06-17 12:27:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        "status": "success",
+        "message": "Welcome to CloudPipelineX Production API!",
+        "version": "1.0.0"
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
