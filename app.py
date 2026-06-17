4979e6cb4c5a37e1c017aef54ab2ed21d699e263
Ishita chaturvedi <168914437+ishita-20-6@users.noreply.github.com>
2026-06-17 13:31:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,58 @@
-from flask import Flask, jsonify
+import os
+import joblib
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return jsonify({
-        "status": "success",
-        "message": "Welcome to CloudPipelineX Production API!",
-        "version": "2.0.0"
-    })
+MODEL_PATH = "iris_model.pkl"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+model = None
+if os.path.exists(MODEL_PATH):
+    model = joblib.load(MODEL_PATH)
+    print("✅ ML Model loaded successfully!")
+else:
+    print("❌ Model file not found! Please run train_model.py first.")
+
+
+@app.route("/")
+def home():
+    return jsonify(
+        {
+            "status": "success",
+            "message": "Welcome to Iris Flower Prediction MLOps API!",
+            "version": "3.0.0",
+        }
+    )
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if not model:
+        return jsonify({"error": "Model not loaded properly"}), 500
+
+    try:
+        data = request.get_json()
+
+        features = [
+            [
+                data["sepal_length"],
+                data["sepal_width"],
+                data["petal_length"],
+                data["petal_width"],
+            ]
+        ]
+        prediction = model.predict(features)[0]
+
+        flower_types = ["Setosa", "Versicolor", "Virginica"]
+        predicted_flower = flower_types[prediction]
+
+        return jsonify(
+            {"status": "success", "predicted_flower_type": predicted_flower}
+        )
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
